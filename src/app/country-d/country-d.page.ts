dcf57fcb508adb06b429d43bad72726d5a896b23
Michal Kurzal <michalkurzal200@gmail.com>
2020-06-28 09:48:58 +0200

    .

--- src/app/country-d/country-d.page.ts
+++ src/app/country-d/country-d.page.ts
@@ -9,6 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 export class CountryDPage implements OnInit {
 cc : any;
 cn :any;
+ncon :any;
+nd: any;
+nr:any;
+tc:any;
+tr: any;
+td:any;
   constructor(private route: ActivatedRoute) {
    
    }
@@ -25,6 +31,12 @@ code =  () =>{
 let c = params["country"];
  this.cc = c.CountryCode;
  this.cn = c.Country;
+ this.ncon = c.NewConfirmed;
+ this.nd = c.NewDeaths;
+ this.nr = c.NewRecovered;
+ this.tc = c.TotalConfirmed;
+ this.td = c.TotalDeaths;
+ this.tr = c.TotalRecovered;
  console.log(this.cc);
  console.log(this.cn);
  return await this.cc;
