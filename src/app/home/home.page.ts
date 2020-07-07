2fc7e50f4526a0690ef36484626b2a95d4855393
Michal Kurzal <michalkurzal200@gmail.com>
2020-07-07 02:33:25 +0200

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,7 @@ import { NavigationExtras, ActivatedRoute, Router } from '@angular/router';
 import {File} from '@ionic-native/file/ngx';
 import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
-
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-home',
@@ -20,19 +20,17 @@ countries;
 
   constructor(private appservice: AppserviceService, public nav:NavController, 
      public platform:Platform, public fileTransfer :FileTransfer, private WebView : WebView,
-    public navParams: NavParams, private route: ActivatedRoute, private router : Router,private file: File) {}
+    public navParams: NavParams, private route: ActivatedRoute, private router : Router,private file: File,
+    private nativeStorage: NativeStorage) {}
   ngOnInit() {
   this.gurl =  this.WebView.convertFileSrc( this.file.dataDirectory);
-    this.appservice.getGlobal().then(res =>{
-      this.global = res;
-      this.countries = this.global.Countries;
+  this.nativeStorage.getItem('DataCountries').then(res =>{
+      this.countries = res;
       console.log('countries', this.countries);
     })
   }
  
 goforward = (param)=>{
-  let ele = document.getElementById('it');
-
  let navigationExtras: NavigationExtras = {
     queryParams: {
         country: param
