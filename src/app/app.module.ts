fb61a59a24b057e990540ab36b6262e8eaa9cfed
Michal Kurzal <michalkurzal200@gmail.com>
2022-01-21 13:43:25 +0100

    removing unused dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,8 @@ import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { HttpClientModule } from '@angular/common/http'
 import { File } from '@ionic-native/file/ngx'
-import {
-    FileTransfer,
-    FileTransferObject,
-} from '@ionic-native/file-transfer/ngx'
 import { WebView } from '@ionic-native/ionic-webview/ngx'
 import { NativeStorage } from '@ionic-native/native-storage/ngx'
-
 import { AboutPageModule } from './about/about.module'
 import { CountryDPageModule } from './country-d/country-d.module'
 import { DashboardPageModule } from './dashboard/dashboard.module'
@@ -35,8 +30,6 @@ import { DashboardPageModule } from './dashboard/dashboard.module'
     ],
     providers: [
         File,
-        FileTransfer,
-        FileTransferObject,
         StatusBar,
         NavParams,
         SplashScreen,
