3f251d5f51abc13a5c900198ec71d33199ffb0fe
Michal Kurzal <michalkurzal200@gmail.com>
2022-03-02 10:37:01 +0100

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,10 @@
 import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
-import { Graphs1Page } from './chart1/graphs1.page'
-import { Graphs2Page } from './chart2/graphs2.page'
 
 const routes: Routes = [
     {
         path: '',
         loadChildren: './tabs/tabs.module#TabsPageModule',
-    },
-    {
-        path: 'graphs1',
-        component: Graphs1Page,
-    },
-    {
-        path: 'graphs2',
-        component: Graphs2Page,
-    },
-    {
-        path: 'country-d',
-        loadChildren: () =>
-            import('./countryDetail/country-d.module').then(
-                (m) => m.CountryDPageModule
-            ),
-    },
-    {
-        path: 'about',
-        loadChildren: () =>
-            import('./about/about.module').then((m) => m.AboutPageModule),
     },
 ]
 
