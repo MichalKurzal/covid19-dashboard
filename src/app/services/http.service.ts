740fcc54df7b4f4870052709c27596a9daddb88d
Michal Kurzal <michalkurzal200@gmail.com>
2022-04-01 22:28:26 +0200

    http get requests update

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,46 +1,50 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class HttpService {
-  url = 'https://disease.sh/v3/'
-  Url2 = 'https://corona.lmao.ninja/v2/'
+    url = 'https://disease.sh/v3/'
 
-  headers = new HttpHeaders()
-      .set('Cache-Control', 'no-cache')
-      .set('Pragma', 'no-cache')
+    headers = new HttpHeaders()
+        .set('Cache-Control', 'no-cache')
+        .set('Pragma', 'no-cache')
+        .set('Access-Control-Allow-Origin', '*')
+        .set(
+            'Access-Control-Allow-Methods',
+            'GET,POST,PATCH,DELETE,PUT,OPTIONS'
+        )
+        .set('Content-Type', 'application/json')
 
+    constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+    WorldwideData() {
+        return this.http
+            .get(`${this.url}covid-19/all`, { headers: this.headers })
+            .toPromise()
+    }
 
-  WorldwideData() {
-    return this.http
-        .get(`${this.url}covid-19/all`, { headers: this.headers })
-        .toPromise()
-}
+    HistoricalData() {
+        return this.http
+            .get(`${this.url}covid-19/historical/all?lastdays=100`, {
+                headers: this.headers,
+            })
+            .toPromise()
+    }
 
-HistoricalData() {
-    return this.http
-        .get(`${this.url}covid-19/historical/all?lastdays=100`, {
-            headers: this.headers,
-        })
-        .toPromise()
-}
-
-getCountriesData() {
-    return this.http
-        .get(`${this.Url2}countries?sort=country`, {
-            headers: this.headers,
-        })
-        .toPromise()
-}
-HistoricalCountry(country) {
-    return this.http
-        .get(`${this.Url2}historical/${country}?lastdays=100`, {
-            headers: this.headers,
-        })
-        .toPromise()
-}
+    getCountriesData() {
+        return this.http
+            .get(`${this.url}covid-19/countries?sort=country`, {
+                headers: this.headers,
+            })
+            .toPromise()
+    }
+    HistoricalCountry(country) {
+        return this.http
+            .get(`${this.url}covid-19/historical/${country}?lastdays=100`, {
+                headers: this.headers,
+            })
+            .toPromise()
+    }
 }
